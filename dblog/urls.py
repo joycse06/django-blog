f7bd4ffe2aa7eb116d3d997ba246475fae1f6ffe
Joy Nag <joyruet06@gmail.com>
2014-05-08 16:32:47 +0600

    fix assets loading

--- dblog/urls.py
+++ dblog/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
+from dblog.settings.common import *
 
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
@@ -11,4 +12,6 @@ urlpatterns = patterns('',
     # Admin panel and documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('dblog.apps.blogengine.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT}),
 )
