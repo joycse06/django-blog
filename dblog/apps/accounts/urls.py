1c7d64214c3787ae1c0761d02de3149e3012ed75
Joy Nag <joyruet06@gmail.com>
2014-05-09 01:19:35 +0600

    more fields added

--- dblog/apps/accounts/urls.py
+++ dblog/apps/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, patterns, include
-
+from forms import SignupFormExtra
 
 urlpatterns = patterns('',
     (r'^accounts/signup/$',
         'userena.views.signup',
-        {'signup_form': SignupExtraProfileForm}),
+        {'signup_form': SignupFormExtra}),
 )
