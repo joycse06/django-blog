bd3875defc74c2ae5074e9c3fc7a56c6ea042698
Joy Nag <joyruet06@gmail.com>
2014-05-08 14:17:40 +0600

    pro settins updated;

--- dblog/settings/prod.py
+++ dblog/settings/prod.py
@@ -81,29 +81,33 @@ INSTALLED_APPS += (
     'storages',
 )
 
-# See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-# See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
-
-# See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID', '')
-AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY', '')
-AWS_STORAGE_BUCKET_NAME = environ.get('AWS_STORAGE_BUCKET_NAME', '')
-AWS_AUTO_CREATE_BUCKET = True
-AWS_QUERYSTRING_AUTH = False
-
-# AWS cache settings, don't change unless you know what you're doing:
-AWS_EXPIRY = 60 * 60 * 24 * 7
-AWS_HEADERS = {
-    'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIRY,
-        AWS_EXPIRY)
-}
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
-########## END STORAGE CONFIGURATION
+# Commented from here
+#
+# # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
+# STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#
+# # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
+# AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
+#
+# # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
+# AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID', '')
+# AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY', '')
+# AWS_STORAGE_BUCKET_NAME = environ.get('AWS_STORAGE_BUCKET_NAME', '')
+# AWS_AUTO_CREATE_BUCKET = True
+# AWS_QUERYSTRING_AUTH = False
+#
+# # AWS cache settings, don't change unless you know what you're doing:
+# AWS_EXPIRY = 60 * 60 * 24 * 7
+# AWS_HEADERS = {
+#     'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIRY,
+#         AWS_EXPIRY)
+# }
+#
+# # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+# STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+# ########## END STORAGE CONFIGURATION
+#
+# Upto Here
 
 
 ########## COMPRESSION CONFIGURATION
